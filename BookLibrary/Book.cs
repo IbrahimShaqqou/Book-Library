9b029f5f5928d9233ba0bd9caa07485a594e5a21
IbrahimShaqqou <ibraheemshaqqou@gmail.com>
2024-10-08 22:11:03 -0400

    Create a book class to define the structure of a book.

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,11 @@
+namespace BookLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public string Genre { get; set; }
+        public string Isread { get; set; }
+    }
+}
